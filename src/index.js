df2b2565b2bd9d55e8bca725e4682c97bfbb5b72
MilaTuzova <tuzovals@gmail.com>
2021-07-12 05:00:30 +0300

    fixed timer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,88 @@
 import './sass/main.scss';
+import Swal from 'sweetalert2';
+
+const refs = {
+    input: document.querySelector('#date-selector'),
+    btn: document.querySelector('[data-start]'),
+    timer: document.querySelector('.timer'),
+    field: document.querySelectorAll('.field'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
+}
+
+// обновить страницу
+// window.location.reload();
+
+
+refs.btn.setAttribute('disabled', true);
+
+
+const currentDate = Date.now();
+// console.log(currentDate);
+let choiceDate = '';
+
+refs.btn.addEventListener('click', onTimerStart);
+refs.input.addEventListener('input', onChoiceData);
+
+
+function onChoiceData() {
+
+    choiceDate = Date.parse(new Date(refs.input.value)) - (180 * 60 * 1000);
+    // console.log(choiceDate);
+
+    if (choiceDate < currentDate) {
+        // console.log('not correct data');
+        Swal.fire('Please choose a date in the future');
+        refs.input.value = '';
+
+    } else {
+        refs.btn.removeAttribute('disabled');
+    }
+}
+
+
+function onTimerStart() {
+    // console.log('btn click');
+    refs.btn.setAttribute('disabled', true);
+
+    const intervalId = null;
+
+    setInterval(() => {
+
+        if (currentDate === choiceDate) {
+            // console.log('таймер подошел к 0');
+            clearInterval(intervalId);
+            return;
+        }
+
+        // console.log('отсчет времени пошел');
+        // console.log(choiceDate);
+
+        const deltaTime = choiceDate - Date.now();
+        // console.log(deltaTime);
+
+        const timerToEnd = convertMs(deltaTime);
+        // console.log(timerToEnd);
+
+        updateFaceTime(timerToEnd);
+    }, 1000)
+
+}
+
+function updateFaceTime({ days, hours, minutes, seconds }) {
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.minutes.textContent = minutes;
+    refs.seconds.textContent = seconds;
+}
+
+
+function pad(value) {
+    return String(value).padStart(2, '0');
+}
+
 
 function convertMs(ms) {
     // Number of milliseconds per unit of time
@@ -8,17 +92,18 @@ function convertMs(ms) {
     const day = hour * 24;
 
     // Remaining days
-    const days = Math.floor(ms / day);
+    const days = pad(Math.floor(ms / day));
     // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
+    const hours = pad(Math.floor((ms % day) / hour));
     // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
     // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
     return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); //{days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); //{days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
